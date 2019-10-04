d575d7ca59c136074956f031e3527e88f7b6edef
Maciej.P <Pytkens@gmail.com>
2019-10-04 23:38:21 +0200

    uppdate

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,20 @@
 class ArticlesController < ApplicationController
 
   def new
-    @article = Article.new
+    @article = Article.new #Do zmiennej article przypisujemy model Article narazie
+    #wszystkie wartości będą mieć wartość nill
   end
 
 def create
   @article = Article.new(article_params)
+  #Tutaj tworzymy nowy article z danych bedacych w params strony
   if @article.save
     flash[:notice] = "Article was successfully created"
     redirect_to article_path(@article)
+=begin
+Sprawdzamy czy artukul zostal zapisany prostym ifem , jesli operacja sie powiodla zostanie wyswietlona o tym informacja na ekranie, nastepnie jestesmy przkierowani na strone show.html wyswietlajaca utworzony artykul 
+
+=end
   else
     render 'new'
   end
@@ -22,6 +28,11 @@ end
 
 private
   def article_params
+=begin
+to jest dosc wazny temat z tego co sie dowiedzialem chodzi tu o bezpieczenstwo i to
+co moze byc przekazane do paramatrow strony w tym wypadku udostepniamy przekazanie 
+:title i :description, nie do konca rozumiem co robi require , zgaduje ze jest poprostu odniesieniem do moedlu article i danych title description
+=end
     params.require(:article).permit(:title,:description)
   end
 end
