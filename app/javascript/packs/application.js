7122f300a8193d2d35b199efe80b1b1cac485518
Maciej.P <Pytkens@gmail.com>
2019-10-22 10:02:19 +0200

    msg

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("bootstrap/dist/js/bootstrap")
-import "bootstrap"
-import "../stylesheets/application"
+import 'bootstrap'
+import '../stylesheets/application'
 document.addEventListener("turbolinks:load",() =>{
 $('[data-toggle="tooltip"]').tooltip()
 })
