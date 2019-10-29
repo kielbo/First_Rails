56b7c7c898d760928917c74991f57807c3b4ddb5
Maciej.P <Pytkens@gmail.com>
2019-10-29 21:09:47 +0100

    msg

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
 	root 'pages#home'
 	get 'about',to:'pages#about'
+  get 'signup',to:'users#new'
+  resources :users, except:[:new]
 	resources :articles
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For deitails on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
